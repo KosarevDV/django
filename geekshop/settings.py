b87d27e9b4d1842fa9829b2da9ca5486a1a6c71f
KosarevDV <KosarevDV@users.noreply.github.com>
2022-05-18 13:27:50 +0300

    Косарев ДВ, гр. GU_python_1738ДЗ к уроку 1

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--551fgthi^ma$j7wb5=f8oln8h5ab4b4fe!@j8oh0di_+!4eal
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'mainapp',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
