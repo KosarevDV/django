307bf48361324035175c903c52967de37387e482
KosarevDV <KosarevDV@users.noreply.github.com>
2022-05-23 00:31:00 +0300

    Косарев ДВ, гр. GU_python_1738ДЗ к уроку 2

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,11 +2,34 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'user_list': [
+            {
+            'first_name': "Dmitry",'last_name': 'Kosarev', 'age': 35
+        },
+            {
+            'first_name': "Oleg", 'last_name': 'Gazmanov', 'age': 70
+            },
+            {
+            'first_name': "Dima", 'last_name': 'Bilan', 'age': 40
+            }
+
+        ]
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href':'products_all', 'title':'все'},
+        {'href': 'products_home', 'title': 'дом'},
+        {'href': 'products_office', 'title': 'офис'},
+        {'href': 'products_modern', 'title': 'модерн'},
+        {'href': 'products_classic', 'title': 'классика'},
+    ]
+    context = {'links_menu':links_menu}
+
+    return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
